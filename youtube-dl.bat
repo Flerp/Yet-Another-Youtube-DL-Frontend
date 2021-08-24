f41bf3ecdf50a711983391677a87edd83383a3d5
Flerp <Halofreak411@gmail.com>
2021-08-24 02:46:53 -0700

    Fixed terrible url parsing hack
    Fixed the dumb hack I had for parsing the url text file.
    
    Also fixed some formatting and added more info in text

--- youtube-dl.bat
+++ youtube-dl.bat
@@ -1,9 +1,7 @@
 @echo off
-setlocal ENABLEDELAYEDEXPANSION
-set vidx=0
 
 :pythoncheck
-ffmpeg -version > "%configpath%\pythonver.txt"
+python -V > "%configpath%\pythonver.txt"
 set /p pythonver=<"%configpath%\pythonver.txt"
 if "%pythonver%"=="" goto pythonmissing
 del "%configpath%\pythonver.txt"
@@ -11,7 +9,7 @@ goto update
 
 :update
 cls
-Echo Checking for Updates
+Echo Checking for youtube -dl updates
 pip install --upgrade youtube-dl
 
 :createconfigdir
@@ -42,8 +40,6 @@ set /p ffmpegver=<"%configpath%\ffmpegver.txt"
 if "%ffmpegver%"=="" goto ffmpegmissing
 del "%configpath%\ffmpegver.txt"
 goto menu
-
-
 
 :pythonmissing
 cls 
@@ -88,7 +84,7 @@ CHOICE /N /C:12345 /M "Enter Corresponding Menu choice (1, 2, 3, 4, 5)"
 IF ERRORLEVEL ==5 exit
 IF ERRORLEVEL ==4 GOTO editurllist
 IF ERRORLEVEL ==3 GOTO setdlpath
-IF ERRORLEVEL ==2 GOTO updateurlsvar
+IF ERRORLEVEL ==2 GOTO updateurls
 IF ERRORLEVEL ==1 GOTO setsingleurl
 
 :setsingleurl
@@ -97,131 +93,24 @@ set /p yturl="Enter Youtube URL: "
 goto downloadsingleurl
 
 :downloadsingleurl
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%yturl%"
+youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(upload_date)s - %%(title)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%yturl%"
 cls 
 echo ========================================
 echo =               Done!                  =
+echo =   Press any key to return to menu    =
 echo ========================================
 pause >nul
 goto menu
 
-:updateurlsvar
-for /F "tokens=*" %%A in (%configpath%\urls.txt) do (
-    SET /A vidx=!vidx! + 1
-    set url!vidx!=%%A
-)
-set url
-cls 
-goto updateurls
-
 :updateurls
-::Holy shit this is gross don't look at this
-if "%url1%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url1%"
-if "%url2%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url2%"
-if "%url3%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url3%"
-if "%url4%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url4%"
-if "%url5%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url5%"
-if "%url6%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url6%"
-if "%url7%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url7%"
-if "%url8%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url8%"
-if "%url9%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url9%"
-if "%url10%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url10%"
-if "%url11%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url11%"
-if "%url12%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url12%"
-if "%url13%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url13%"
-if "%url14%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url14%"
-if "%url15%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url15%"
-if "%url16%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url16%"
-if "%url17%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url17%"
-if "%url18%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url18%"
-if "%url19%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url19%"
-if "%url20%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url20%"
-if "%url21%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url21%"
-if "%url22%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url22%"
-if "%url23%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url23%"
-if "%url24%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url24%"
-if "%url25%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url25%"
-if "%url26%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url26%"
-if "%url27%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url27%"
-if "%url28%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url28%"
-if "%url29%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url29%"
-if "%url30%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url30%"
-if "%url31%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url31%"
-if "%url32%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url32%"
-if "%url33%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url33%"
-if "%url34%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url34%"
-if "%url35%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url35%"
-if "%url36%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url36%"
-if "%url37%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url37%"
-if "%url38%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url38%"
-if "%url39%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url39%"
-if "%url40%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url40%"
-if "%url41%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url41%"
-if "%url42%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url42%"
-if "%url43%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url43%"
-if "%url44%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url44%"
-if "%url45%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url45%"
-if "%url46%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url46%"
-if "%url47%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url47%"
-if "%url48%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url48%"
-if "%url49%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url49%"
-if "%url50%"=="" goto updatedone
-youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(title)s-%%(upload_date)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt "%url50%"
+youtube-dl.exe -i -o "%dlpath%\%%(uploader)s\%%(upload_date)s - %%(title)s.%%(ext)s" --ignore-config --hls-prefer-native -f "bestvideo+bestaudio/best" --download-archive %configpath%\downloaded_videos_list.txt -a "%configpath%\urls.txt"
 goto updatedone 
 
 :updatedone
 cls
 echo ========================================
 echo =               Done!                  =
+echo =   Press any key to return to menu    =
 echo ========================================
 pause >nul
 goto menu
